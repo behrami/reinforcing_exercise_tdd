3b59a5a13acfab1cf0af9a31f12f6d60e38b6edd
behrami <behram1993@gmail.com>
2018-05-01 12:41:45 -0400

    done

---
+++ calculator.rb
@@ -0,0 +1,19 @@
+# class Calculator
+
+  def add(num1, num2)
+    return num1 + num2
+  end
+
+  def subtract(num1, num2)
+    return num1 - num2
+  end
+
+  def sum(array)
+    total = 0
+    array.each do |num|
+      total += num
+    end
+    return total
+  end
+
+# end
